00f29d6bfcd53383ccce7a92aed6fda570fdbeac
Raaj Ali <raajrali1999@gmail.com>
2024-12-19 09:52:13 -0600

    All 5 Categories Fixed

--- index.js
+++ index.js
@@ -6,11 +6,52 @@ const genderChar = document.querySelector("#gender");
 const speciesChar = document.querySelector("#species");
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5")
+    fetch("https://rickandmortyapi.com/api/character/1,2,6,13,16")
       .then(response => response.json())
       .then(characters => {
           characters.forEach(objCharacters => showCharacter(objCharacters));
-          //console.log(characters[0])
           displayCharacters(characters[0]);
         
         })});
+
+        characterBar.addEventListener("mouseover", (event) => {
+            event.target.style.color = "blue";
+        });
+        characterBar.addEventListener("mouseout", (event) => {
+                event.target.style.color = "black";
+        });
+        function showCharacter(objCharacter) {
+            const img = document.createElement("span");
+            img.textContent = objCharacter.name;
+            img.addEventListener("click", (e) => displayCharacters(objCharacter));
+            characterBar.append(img);
+        }
+        function displayCharacters(objCharacters) {
+            cuteNames.setAttribute("data-id", objCharacters.id);
+            cuteNames.textContent = objCharacters.name;
+            statusChar.textContent = objCharacters.status;
+            genderChar.textContent = objCharacters.gender;
+           speciesChar.textContent = objCharacters.species;
+           charactersImage.src = objCharacters.image
+           charactersImage.addEventListener('click', alertFunction);
+        
+        }
+        function alertFunction() {
+            alert("Likes")
+           }
+        document.querySelector("#votes-form").addEventListener("submit", (e) =>{
+            e.preventDefault();
+            let copyVotes = e.target.votes.value
+            console.log(copyVotes);
+            handleVotes(copyVotes);
+        })
+        function handleVotes(votesAdded, currentVotes = parseInt(document.querySelector("#vote").innerText)){
+            currentVotes += parseInt(votesAdded);
+            let voteDisplay = document.querySelector("#vote");
+            voteDisplay.textContent = currentVotes;
+            console.log(currentVotes);
+        }
+        document.querySelector("#reset-btn").addEventListener("click", (event) => {
+            handleVotes(0, 0);
+        })
+        
