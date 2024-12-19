c9371fca499a3ff14bca6fc1f672472508a42efa
Raaj Ali <raajrali1999@gmail.com>
2024-12-19 03:00:23 -0600

    added 1 Event Listener

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const characterBar = document.getElementById("character-bar");
 const charactersImage = document.querySelector("#image");
-const cuteNames = document.querySelector("#image");
-const statusChar = document.querySelector("#image");
-const genderChar = document.querySelector("#image");
-const speciesChar = document.querySelector("#image");
+const cuteNames = document.querySelector("#name");
+const statusChar = document.querySelector("#vote-count");
+const genderChar = document.querySelector("#gender");
+const speciesChar = document.querySelector("#species");
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("https://rickandmortyapi.com/api/character/1,2,3,4,5")
+      .then(response => response.json())
+      .then(characters => {
+          characters.forEach(objCharacters => showCharacter(objCharacters));
+          //console.log(characters[0])
+          displayCharacters(characters[0]);
+        
+        })});
