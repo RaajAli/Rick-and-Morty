41873ee30358bf39e0bcd4d08e4c4a4a3831fe7d
Raaj Ali <raajrali1999@gmail.com>
2024-12-19 02:30:05 -0600

    Query Selectors added

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code here:
+const characterBar = document.getElementById("character-bar");
+const charactersImage = document.querySelector("#image");
+const cuteNames = document.querySelector("#image");
+const statusChar = document.querySelector("#image");
+const genderChar = document.querySelector("#image");
+const speciesChar = document.querySelector("#image");
